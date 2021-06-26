e41b7b916705b4a93e381610f0dbe6ba2469c869
guianderson <guilherme.barbosa34@outlook.com>
2021-06-25 23:21:35 -0300

    Correções no controller e nova rota de download por nome de arquivo

--- Collector/app/controllers/routes.py
+++ Collector/app/controllers/routes.py
@@ -2,20 +2,19 @@ import os
 from flask import render_template
 from app import app
 from ftplib import FTP
-from datetime import date
 
-from app.models.forms import DataForm
+# TODO: Criar arquivo para armazenas as variaveis de ambiente
+dir_name = '/terrama2q/TerraMA2Q_408'
+ftp_adress = 'ftp.dgi.inpe.br'
+user_info = ('queimadas', 'inpe_2012')
+
+conn = FTP(ftp_adress)
+conn.login(*user_info)
+conn.cwd(dir_name)
 
 
 @app.route("/collector")
 def collector():
-    dir_name = '/terrama2q/TerraMA2Q_408'
-    ftp_adress = 'ftp.dgi.inpe.br'
-    user_info = ('queimadas', 'inpe_2012')
-
-    conn = FTP(ftp_adress)
-    conn.login(*user_info)
-    conn.cwd(dir_name)
 
     log = []
     files_list = []
@@ -31,10 +30,22 @@ def collector():
         files_list.append(d)
     del (files_list[0:2])
 
-    return render_template('teste.html'), str(files_list)
+    return str(files_list)
+
+
+@app.route("/collector/download/<nome_arquivo>")
+def download_by_name(nome_arquivo):
+    nonpassive = False
+
+    print(conn.retrlines('LIST'))
+
+    if nonpassive:
+        conn.set_pasv(False)
+
+    return conn.retrbinary("RETR " + nome_arquivo, open(nome_arquivo, "wb").write)
 
 
 @app.route("/")
 @app.route("/index")
-def download():
+def index():
     return render_template('index.html')
