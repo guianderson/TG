baa7ad0e64a81d13d859ea4455655b39d4ba0a1b
guianderson <guilherme.barbosa34@outlook.com>
2021-06-29 23:49:52 -0300

    logica para que quando for mês de fevereiro o dia máximo seja igual a 28

--- Collector/app/controllers/routes.py
+++ Collector/app/controllers/routes.py
@@ -58,55 +58,58 @@ def data(dt_inicial):
     file = 'focos_terrama2q_' + ano_inicial + '' + mes_inicial + '' + dia_inicial + '_'
     full_file = []
 
-    for d in range(0, 60, 10):
-        if d < 10:
-            full_file.append(file + "000" + str(d) + '.csv')
-        elif 10 <= d <= 90:
-            full_file.append(file + "00" + str(d) + '.csv')
-    for d in range(100, 160, 10):
-        full_file.append(file + "0" + str(d) + '.csv')
-    for d in range(200, 260, 10):
-        full_file.append(file + "0" + str(d) + '.csv')
-    for d in range(300, 360, 10):
-        full_file.append(file + "0" + str(d) + '.csv')
-    for d in range(400, 460, 10):
-        full_file.append(file + "0" + str(d) + '.csv')
-    for d in range(500, 560, 10):
-        full_file.append(file + "0" + str(d) + '.csv')
-    for d in range(600, 660, 10):
-        full_file.append(file + "0" + str(d) + '.csv')
-    for d in range(700, 760, 10):
-        full_file.append(file + "0" + str(d) + '.csv')
-    for d in range(800, 860, 10):
-        full_file.append(file + "0" + str(d) + '.csv')
-    for d in range(900, 960, 10):
-        full_file.append(file + "0" + str(d) + '.csv')
-    for d in range(1000, 1060, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(1100, 1160, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(1200, 1260, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(1300, 1360, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(1400, 1460, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(1500, 1560, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(1600, 1660, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(1700, 1760, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(1800, 1860, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(1900, 1960, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(2000, 2060, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(2100, 2160, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(2200, 2260, 10):
-        full_file.append(file + str(d) + '.csv')
-    for d in range(2300, 2360, 10):
-        full_file.append(file + str(d) + '.csv')
-    return "<h1>Lista de Arquivos do dia: " +dia_inicial + "/" + mes_inicial + "/" + ano_inicial + "</h1>" + str(full_file)
+    if str(mes_inicial) == str('02') and str(dia_inicial) > str(28):
+        return "<h1>Data invalida</h1>"
+    else:
+        for d in range(0, 60, 10):
+            if d < 10:
+                full_file.append(file + "000" + str(d) + '.csv')
+            elif 10 <= d <= 90:
+                full_file.append(file + "00" + str(d) + '.csv')
+        for d in range(100, 160, 10):
+            full_file.append(file + "0" + str(d) + '.csv')
+        for d in range(200, 260, 10):
+            full_file.append(file + "0" + str(d) + '.csv')
+        for d in range(300, 360, 10):
+            full_file.append(file + "0" + str(d) + '.csv')
+        for d in range(400, 460, 10):
+            full_file.append(file + "0" + str(d) + '.csv')
+        for d in range(500, 560, 10):
+            full_file.append(file + "0" + str(d) + '.csv')
+        for d in range(600, 660, 10):
+            full_file.append(file + "0" + str(d) + '.csv')
+        for d in range(700, 760, 10):
+            full_file.append(file + "0" + str(d) + '.csv')
+        for d in range(800, 860, 10):
+            full_file.append(file + "0" + str(d) + '.csv')
+        for d in range(900, 960, 10):
+            full_file.append(file + "0" + str(d) + '.csv')
+        for d in range(1000, 1060, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(1100, 1160, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(1200, 1260, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(1300, 1360, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(1400, 1460, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(1500, 1560, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(1600, 1660, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(1700, 1760, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(1800, 1860, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(1900, 1960, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(2000, 2060, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(2100, 2160, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(2200, 2260, 10):
+            full_file.append(file + str(d) + '.csv')
+        for d in range(2300, 2360, 10):
+            full_file.append(file + str(d) + '.csv')
+        return "<h1>Lista de Arquivos do dia: " +dia_inicial + "/" + mes_inicial + "/" + ano_inicial + "</h1>" + str(full_file)
